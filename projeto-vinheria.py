e2cfad8a25961d4593af75049efc91318b6fc954
JuanPinheiroFIAP <rm552202@fiap.com.br>
2023-04-13 16:51:23 -0300

    segundo_commit

--- projeto-vinheria.py
+++ projeto-vinheria.py
@@ -1,47 +1,40 @@
-# print("Sejá Bem vindo ao nosso site!")
-# print("Olá vamos efetuar seu cadastro no nosso site")
-# nome = input("Me informe seu nome completo: ")
+print("Sejá Bem Vindo!\n\nVamos efetuar seu cadastro!")
+print()
+nome = input("Me informe seu nome completo: ")
+print()
 idade = int(input("Me informe sua idade: "))
-# endereço_cliente = input("Me informe seu endereço: ")
-# endereço_entrega = input("Me informe seu endereço entrega: ")
-frete = 50
+print()
+endereço_cliente = input("Me informe seu endereço: ")
+print()
+endereço_entrega = input("Me informe seu endereço entrega: ")
+frete = 50.0
 valor = 0.0
-vinho_tinto = 50.0
-vinho_branco = 40.0
-vinho_rosé = 45.0
-prosecco = 60.0
-champagne = 100.0
-
 catalogo_vinhos = {
         'vinho tinto': 50.0,
         'vinho branco': 40.0,
         'vinho rosé': 45.0,
         'prosecco': 60.0,
         'champagne': 100.0
-    }
+}
 quantidades = {}
-
 if idade >= 18:
-    while True:
-        opcao = input("Deseja comprar algum item nosso? (S/N)")
-        if opcao.upper() == "S":
-            print(catalogo_vinhos)
-
-            for opcao in catalogo_vinhos:
-                quantidade = int(input(f"Quantas garrafas de {opcao}"))
-                quantidades[opcao] = quantidade
-
-            for opcao, quantidade in quantidades.items():
-                preco = catalogo_vinhos[opcao]
-                valor += preco * quantidade
-        elif opcao.upper() == "N":
-            print("Compra finalizada!")
-            break
-        else:
-            print("Opção invalida")
-
-
-
+    print(f"Prontinho seu cadastro está feito.\nAqui está nosso catalogo de vinhos: {catalogo_vinhos.items()}")
+    for opcao in catalogo_vinhos:
+        while True:
+            quantidade = input(f"Quantas garrafas de {opcao} você deseja comprar? ")
+            if quantidade.isdigit():
+                quantidades[opcao] = int(quantidade)
+                break
+            else:
+                print("Quantidade inválida. Por favor, digite um número inteiro válido.")
+    for opcao, quantidade in quantidades.items():
+        preco = catalogo_vinhos[opcao]
+        valor += preco * quantidade
+        print(f"Sua compra deu R${valor:.2f}")
+    if valor <= 100:
+        print(f"Seu frete foi de R$: {frete}")
+    else:
+        print("Frete gratis")
 else:
     print("Sinto muito mas o comprador tem que ser maior de idade.")
     print("Até mais!")
